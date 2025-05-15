1b629512121173df1b276c7f1a8d748136915436
BhumikaMaheshwari05 <maheshwaribhumika82@gmail.com>
2025-05-16 01:07:05 +0530

    update code

--- controllers/property.js
+++ controllers/property.js
@@ -24,46 +24,32 @@ module.exports.showProperty=async (req,res)=>{
     }
   }
 
-const geocode = require('../utils/geocode');
-
 module.exports.addProperty = async (req, res, next) => {
     try {
         let url = req.file.path;
         let filename = req.file.filename;
         let { title, description, price, location, country } = req.body;
 
-        // Get coordinates for the location
-        const coords = await geocode(location);
-
-        if (!coords) {
-            req.flash('error', 'Could not find location on map. Please check your location.');
-
-            // Redirect safely: if referer exists, redirect there; otherwise, fallback to /listings
-            return res.redirect(req.get('referer') || '/listings');
-        }
-
+        
         const newListing = new Listing({
             title,
             description,
             price,
             location,
             country,
-            latitude: coords.latitude,
-            longitude: coords.longitude,
             image: { url, filename }
         });
 
         newListing.owner = req.user._id;
-
         await newListing.save();
 
         req.flash('success', 'New Listing Created');
         res.redirect('/listings');
     } catch (err) {
-        next(err);
+        req.flash('error', 'Failed to create listing. Please try again.');
+        res.redirect('/listings');
     }
 };
-
 
   module.exports.renderEditForm=async (req,res)=>{
     let {id}=req.params
